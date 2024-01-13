f13aa04e0cd948a2794d7572b49ff6c50fafdd42
Adet <adet@Adetias-MacBook-Pro.local>
2024-01-14 05:44:59 +0700

    init project spring security 2

---
+++ springsecurity2/src/main/java/com/github/adet/Springsecurity2Application.java
@@ -0,0 +1,13 @@
+package com.github.adet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecurity2Application.class, args);
+	}
+
+}
---
+++ springsecurity2/src/test/java/com/github/adet/Springsecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecurity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
