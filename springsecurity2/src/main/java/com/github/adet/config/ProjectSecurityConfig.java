de1ea5ea15e09743771df831e6391c5b6cf304de
Adet <adet@Adetias-MacBook-Pro.local>
2024-01-14 14:46:14 +0700

    user based on InMemoryUserDetailsManager

--- springsecurity2/src/main/java/com/github/adet/config/ProjectSecurityConfig.java
+++ springsecurity2/src/main/java/com/github/adet/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,5 +35,23 @@ public class ProjectSecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .authorities("user")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
