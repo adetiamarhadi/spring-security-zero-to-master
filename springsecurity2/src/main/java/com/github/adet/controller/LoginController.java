bba3181b7233c4e5df31d5ac93adc2f9489f545c
Adet <adet@Adetias-MacBook-Pro.local>
2024-01-25 16:13:15 +0700

    bcrypt password encoder implementation

--- springsecurity2/src/main/java/com/github/adet/controller/LoginController.java
+++ springsecurity2/src/main/java/com/github/adet/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.github.adet.repository.CustomersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     private final CustomersRepository customersRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public LoginController(CustomersRepository customersRepository) {
+    public LoginController(CustomersRepository customersRepository,
+                           PasswordEncoder passwordEncoder) {
         this.customersRepository = customersRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customers customers) {
         try {
+
+            String encodedPassword = this.passwordEncoder.encode(customers.getPwd());
+
+            customers.setPwd(encodedPassword);
+
             this.customersRepository.save(customers);
+
             return ResponseEntity.status(HttpStatus.CREATED).body("successfully created");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
